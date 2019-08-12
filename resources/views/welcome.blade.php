41088316e6f431bfc97b2b285283d174ff18baf5
Mark Williams <williams.mark81@outlook.com>
2019-08-12 17:09:26 +0100

    Updated links and aria-labels

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,64 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Neebles!</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="css/app.css" rel="stylesheet">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -85,12 +34,12 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">About</a>
-                    <a href="https://laracasts.com">Gallery</a>
-                    <a href="https://laravel-news.com">Portfolio</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Contact</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/about" aria-label="Read more all about me!">About</a>
+                    <a href="/gallery" aria-label="Did I mention I enjoy photography?">Gallery</a>
+                    <a href="/portfolio" aria-label="Read more about some of my work, not just look at it">Portfolio</a>
+                    <a href="/blog" aria-label="Read more about my general ramblings">Blog</a>
+                    <a href="/contact"aria-label="Get in touch with me">Contact</a>
+                    <a href="https://github.com/auntynigel" target="_blank" aria-label="Go to my github repo">GitHub</a>
                 </div>
             </div>
         </div>
