44aacfcbab845c02868eb30428d21f18fc9105bf
Mark Williams <williams.mark81@outlook.com>
2019-08-13 11:43:43 +0100

    header work and flexbox

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Gallery
+
+Run "npm run development -- --watch" to compile JS and SASS on the fly
+
+Run "php artisan serve" to run server
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,34 +8,31 @@
     <link href="css/app.css" rel="stylesheet">
   </head>
   <body>
-    <div class="flex-top">
-      @if (Route::has('login'))
-          <div class="top-right links">
-              @auth
-                  <a href="{{ url('/home') }}">Home</a>
-              @else
-                  <a href="{{ route('login') }}">Login</a>
-
-                  @if (Route::has('register'))
-                      <a href="{{ route('register') }}">Register</a>
-                  @endif
-              @endauth
-          </div>
-      @endif
-  </div>
-  <div class="flex-top header">
-    <div class="clear">
-      <h2>Mark</h2>
+    <div class="flex-header">
+    <div class="flex-nav links">
+      <img src="images/neebles.png" alt="Logo of a cute monster" height="100px">
     </div>
-    <div class="clear links">
+      <div class="flex-nav title">
+        <h2>Mark</h2>
+      </div>
+      <div class="flex-nav links">
         <a href="/about" aria-label="Read more all about me!">About</a>
+      </div>
+      <div class="flex-nav links">
         <a href="/gallery" aria-label="Did I mention I enjoy photography?">Gallery</a>
+      </div>
+      <div class="flex-nav links">
         <a href="/portfolio" aria-label="Read more about some of my work, not just look at it">Portfolio</a>
+      </div>
+      <div class="flex-nav links">
         <a href="/blog" aria-label="Read more about my general ramblings">Blog</a>
+      </div>
+      <div class="flex-nav links">
         <a href="/contact"aria-label="Get in touch with me">Contact</a>
+      </div>
+      <div class="flex-nav links">
         <a href="https://github.com/auntynigel" target="_blank" aria-label="Go to my github repo">GitHub</a>
+      </div>
     </div>
-
-  </div>
   </body>
 </html>
